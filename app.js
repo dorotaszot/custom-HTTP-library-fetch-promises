1ff0c38f6d2a7026cd381ccf97072a3d90c9f3b5
dorotaszot <62651157+dorotaszot@users.noreply.github.com>
2020-09-14 15:06:56 +0200

    POST method done

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 const http = new EasyHTTP;
 
-http.get('https://jsonplaceholder.typicode.com/users')
+// http.get('https://jsonplaceholder.typicode.com/users')
+//   .then(data => console.log(data))
+//   .catch(err => console.log(err))
+
+// User data
+const data = {
+  name: 'Tom Smith',
+  username: 'tomsm',
+  email: 'toms@gmail.com'
+}
+
+// Create Post
+http.post('https://jsonplaceholder.typicode.com/users', data)
   .then(data => console.log(data))
   .catch(err => console.log(err))
+
 
--- easyhttp.js
+++ easyhttp.js
@@ -7,5 +7,20 @@ class EasyHTTP {
         .catch(err => reject(err))
     })
   }
+
+  post(url, data) {
+    return new Promise((resolve, reject) => {
+      fetch(url, {
+        method: 'POST',
+        headers: {
+          'Content-type': 'application/json'
+        },
+        body: JSON.stringify(data)
+      })
+        .then(res => res.json())
+        .then(data => resolve(data))
+        .catch(err => reject(data))
+    })
+  }
 }
 
